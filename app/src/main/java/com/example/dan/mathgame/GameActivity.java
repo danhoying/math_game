266af1b37c706ad3c11ade4b08693c02773a0674
Dan Hoying <danhoying@gmail.com>
2017-07-27 13:48:17 -0700

    Initialize UI element variables

--- app/src/main/java/com/example/dan/mathgame/GameActivity.java
+++ app/src/main/java/com/example/dan/mathgame/GameActivity.java
@@ -1,7 +1,9 @@
 package com.example.dan.mathgame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        int partA = 9;
+        int partB = 9;
+        int correctAnswer = partA + partB;
+        int wrongAnswer1 = correctAnswer - 1;
+        int wrongAnswer2 = correctAnswer + 1;
+
+        TextView textObjectPartA = (TextView) findViewById(R.id.textPartA);
+        TextView textObjectPartB = (TextView) findViewById(R.id.textPartB);
+
+        Button buttonObjectChoice1 = (Button) findViewById(R.id.buttonChoice1);
+        Button buttonObjectChoice2 = (Button) findViewById(R.id.buttonChoice2);
+        Button buttonObjectChoice3 = (Button) findViewById(R.id.buttonChoice3);
+
     }
 }
