525f5d35e7ca3acbe3111a211a1f00dcb7b82b09
Dan Hoying <danhoying@gmail.com>
2017-07-27 13:53:46 -0700

    Set text values for UI elements

--- app/src/main/java/com/example/dan/mathgame/GameActivity.java
+++ app/src/main/java/com/example/dan/mathgame/GameActivity.java
@@ -18,6 +18,11 @@ public class GameActivity extends AppCompatActivity {
         int wrongAnswer1 = correctAnswer - 1;
         int wrongAnswer2 = correctAnswer + 1;
 
+        /*Here we get a working object based on either the button
+        or TextView class and base as well as link our new objects
+        directly to the appropriate UI elements that we created
+        previously*/
+
         TextView textObjectPartA = (TextView) findViewById(R.id.textPartA);
         TextView textObjectPartB = (TextView) findViewById(R.id.textPartB);
 
@@ -25,5 +30,19 @@ public class GameActivity extends AppCompatActivity {
         Button buttonObjectChoice2 = (Button) findViewById(R.id.buttonChoice2);
         Button buttonObjectChoice3 = (Button) findViewById(R.id.buttonChoice3);
 
+        //Now we use the setText method of the class on our objects
+        //to show our variable values on the UI elements.
+        //Just like when we output to the console in the exercise -
+        //Expressions in Java, only now we use setText method
+        //to put the values in our variables onto the actual UI.
+
+        textObjectPartA.setText("" + partA);
+        textObjectPartB.setText("" + partB);
+
+        //which button receives which answer at this stage is arbitrary.
+
+        buttonObjectChoice1.setText("" + correctAnswer);
+        buttonObjectChoice2.setText("" + wrongAnswer1);
+        buttonObjectChoice3.setText("" + wrongAnswer2);
     }
 }
