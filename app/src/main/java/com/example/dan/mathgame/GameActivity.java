7b535ceebad77afe74e7a56095eebda3fa466551
Dan Hoying <danhoying@gmail.com>
2017-07-28 14:31:40 -0700

    Fix Game layout and implement answer checking

--- app/src/main/java/com/example/dan/mathgame/GameActivity.java
+++ app/src/main/java/com/example/dan/mathgame/GameActivity.java
@@ -2,10 +2,17 @@ package com.example.dan.mathgame;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+
+    int correctAnswer;
+    Button buttonObjectChoice1;
+    Button buttonObjectChoice2;
+    Button buttonObjectChoice3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +21,7 @@ public class GameActivity extends AppCompatActivity {
 
         int partA = 9;
         int partB = 9;
-        int correctAnswer = partA + partB;
+        correctAnswer = partA * partB;
         int wrongAnswer1 = correctAnswer - 1;
         int wrongAnswer2 = correctAnswer + 1;
 
@@ -26,9 +33,9 @@ public class GameActivity extends AppCompatActivity {
         TextView textObjectPartA = (TextView) findViewById(R.id.textPartA);
         TextView textObjectPartB = (TextView) findViewById(R.id.textPartB);
 
-        Button buttonObjectChoice1 = (Button) findViewById(R.id.buttonChoice1);
-        Button buttonObjectChoice2 = (Button) findViewById(R.id.buttonChoice2);
-        Button buttonObjectChoice3 = (Button) findViewById(R.id.buttonChoice3);
+        buttonObjectChoice1 = (Button) findViewById(R.id.buttonChoice1);
+        buttonObjectChoice2 = (Button) findViewById(R.id.buttonChoice2);
+        buttonObjectChoice3 = (Button) findViewById(R.id.buttonChoice3);
 
         //Now we use the setText method of the class on our objects
         //to show our variable values on the UI elements.
@@ -44,5 +51,45 @@ public class GameActivity extends AppCompatActivity {
         buttonObjectChoice1.setText("" + correctAnswer);
         buttonObjectChoice2.setText("" + wrongAnswer1);
         buttonObjectChoice3.setText("" + wrongAnswer2);
+
+        buttonObjectChoice1.setOnClickListener(this);
+        buttonObjectChoice2.setOnClickListener(this);
+        buttonObjectChoice3.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        int answerGiven;
+        switch (view.getId()) {
+            case R.id.buttonChoice1:
+                answerGiven = Integer.parseInt("" + buttonObjectChoice1.getText());
+                if (answerGiven == correctAnswer) {
+                    Toast.makeText(getApplicationContext(), "Well done!",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Sorry, that's wrong",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.buttonChoice2:
+                answerGiven = Integer.parseInt("" + buttonObjectChoice2.getText());
+                if (answerGiven == correctAnswer) {
+                    Toast.makeText(getApplicationContext(), "Well done!",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Sorry, that's wrong",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.buttonChoice3:
+                answerGiven = Integer.parseInt("" + buttonObjectChoice3.getText());
+                if (answerGiven == correctAnswer) {
+                    Toast.makeText(getApplicationContext(), "Well done!",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Sorry, that's wrong",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 }
